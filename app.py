4559ec319722d9abab7864b17cbe2a75968f57f1
WILLIAM <gilclavijowilliam@gmail.com>
2023-06-17 18:33:13 -0500

    Home Spotify

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask, render_template, request, url_for, redirect
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET', 'POST'])
+def indexx():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
